9f10178ae30082337f063b5892d15c916fdb7c1c
kai-dg <km.commissions@gmail.com>
2018-10-20 21:13:40 +0000

    added function files, renamed & modified printf.c

---
+++ _unknown.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/*
+ * _char - prints a character
+ * @ch:
+ * Return - printed char
+*/
+int _unknown (va_list ch);
+{
+        return (_putchar(va_arg(ch,int)));
+}
---
+++ _unsigned.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/*
+ * _char - prints a character
+ * @ch:
+ * Return - printed char
+*/
+int _unsigned (va_list ch);
+{
+        return (_putchar(va_arg(ch,int)));
+}
+
